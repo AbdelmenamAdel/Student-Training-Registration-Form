04d1ead56f20460eb515ac668f0a2d436a08d179
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-21 02:45:00 +0300

    update comments

--- SQL/database.py
+++ SQL/database.py
@@ -6,20 +6,20 @@ db = sqlite3.connect("SQL/Database/app.db")
 # ! Database Cursor
 cursor = db.cursor()
 # ! CRUD Operations
-# ! Create Table
+# ! Create Users Table 
 db.execute("""CREATE TABLE IF NOT EXISTS Users
     (uid INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL)""")
 db.commit()
-
+# ! Create Courses Table
 db.execute("""CREATE TABLE IF NOT EXISTS Courses
     (cid INTEGER PRIMARY KEY AUTOINCREMENT,
     cname VARCHAR(255) NOT NULL,
     cdescription VARCHAR(255) NOT NULL)""")
 db.commit()
-
+# ! Create Students Table
 db.execute("""CREATE TABLE IF NOT EXISTS Students
     (sid INTEGER PRIMARY KEY AUTOINCREMENT,
     fname VARCHAR(255) NOT NULL,
@@ -29,6 +29,7 @@ db.execute("""CREATE TABLE IF NOT EXISTS Students
     sgpa Integer(5) NOT NULL,
     scourse VARCHAR(255) NOT NULL)""")
 db.commit()
+# ! Create Teachers Table
 db.execute("""CREATE TABLE IF NOT EXISTS Teachers
     (tid INTEGER PRIMARY KEY AUTOINCREMENT,
     tname VARCHAR(255) NOT NULL,
