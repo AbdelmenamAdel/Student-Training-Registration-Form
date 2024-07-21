f6986766683949369040b0169ea51ce20b77d245
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-22 02:47:09 +0300

    Add StudentModel class to SQL/Models directory

---
+++ SQL/Models/student_model.py
@@ -0,0 +1,9 @@
+class StudentModel:
+    def __init__(self, sid, fname, mname, lname, semail, sgpa, scourse):
+        self.sid = sid
+        self.fname = fname
+        self.mname = mname
+        self.lname = lname
+        self.semail = semail
+        self.sgpa = sgpa
+        self.scourse = scourse
