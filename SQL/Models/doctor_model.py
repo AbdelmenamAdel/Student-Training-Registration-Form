91d8f2d853529c1eceaf7ca33413e5eb782aa716
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-22 02:47:01 +0300

    Add DoctorModel class to SQL Models directory

---
+++ SQL/Models/doctor_model.py
@@ -0,0 +1,6 @@
+class DoctorModel:
+    def __init__(self, tid, tname, temail, tcourse):
+        self.tid = tid
+        self.tname = tname
+        self.temail = temail
+        self.tcourse = tcourse
