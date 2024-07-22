6585217e472a578257e3ed1604585d06a47192a7
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-22 03:10:30 +0300

    Add GUI constants file with sizes, fonts, and colors.

---
+++ GUI/Constants/constants.py
@@ -0,0 +1,15 @@
+
+#!############################## constants #!##############################
+# ! Sizes
+padx=20
+pady=20
+login_geometry="450x250"
+# ! Fonts
+font_style_arial="Arial"
+font_size_12=12
+
+# ! Colors
+background_color="#e0e0e0"
+primary_color="#189df6"
+black="#333333"
+white="#ffffff"
