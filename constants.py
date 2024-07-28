fbedbd255d7e5e3d77a1a13b465c8eb2d03a15ae
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-28 14:58:14 +0300

    const

---
+++ constants.py
@@ -0,0 +1,17 @@
+
+#!############################## constants #!##############################
+# ! Sizes
+padx=20
+pady=20
+width=750
+height=500
+geometry=f"{width}x{height}"
+# ! Fonts
+font_style_arial="Arial"
+font_size_12=12
+
+# ! Colors
+background_color="#e0e0e0"
+primary_color="#189df6"
+black="#333333"
+white="#ffffff"
