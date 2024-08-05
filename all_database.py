584bf7e35d70cb9eb375dca3cec298fcc6447997
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-05 16:37:14 +0300

    update in all db

--- all_database.py
+++ all_database.py
@@ -2,7 +2,7 @@ import sqlite3
 # ! ##################################### Create Operation #####################################
 def create_db():
     # ! Database Connection
-    db = sqlite3.connect("Database/registration_form_project.db")
+    db = sqlite3.connect("Database/student_registration_system.db")
     # ! Settings Up The Cursor
     cr = db.cursor()
     # ! CRUD (Create, Read, Update, Delete) Operations
@@ -16,9 +16,10 @@ def create_db():
     # ! Create Courses Table
     cr.execute("""CREATE TABLE IF NOT EXISTS Courses
         (cid INTEGER PRIMARY KEY AUTOINCREMENT,
-        cname VARCHAR(255) NOT NULL,
-        code VARCHAR(255) NOT NULL,
-        hours INTEGER NOT NULL)""")
+        name VARCHAR(100) NOT NULL,
+        code VARCHAR(20) NOT NULL,
+        hours INTEGER NOT NULL,
+        grade INTEGER NOT NULL)""")
     # ! Create Students Table
     cr.execute("""CREATE TABLE IF NOT EXISTS Students
         (sid INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,8 +27,12 @@ def create_db():
         password VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
         Ssh INTEGER NOT NULL,
-        gpa INTEGER NOT NULL,
+        gpa REAL NOT NULL,
         grade VARCHAR(255) NOT NULL,       
         passedHours INTEGER NOT NULL)""")
+    cr.execute("""CREATE TABLE IF NOT EXISTS Regestered
+        (rid INTEGER PRIMARY KEY AUTOINCREMENT,
+        subject_id INTEGER NOT NULL,
+        student_id INTEGER NOT NULL)""")
 
 create_db()
